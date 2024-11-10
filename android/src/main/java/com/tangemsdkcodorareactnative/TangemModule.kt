2174d16eaf618ba73b99ce5393a9ca05ba8baa49
C0C41N <51693606+C0C41N@users.noreply.github.com>
2024-11-10 12:23:21 +0500

    chore: update

--- android/src/main/java/com/tangemsdkcodorareactnative/TangemModule.kt
+++ android/src/main/java/com/tangemsdkcodorareactnative/TangemModule.kt
@@ -5,11 +5,14 @@ import com.tangem.TangemSdk
 import com.tangem.common.core.TangemError
 
 interface TangemModule {
+
     var sdk: TangemSdk
+
     fun handleReject(promise: Promise, err: TangemError) {
         promise.reject(
           err.customMessage,
           err::class.simpleName!!.replaceFirstChar { it.lowercase() }
         )
     }
+
 }
