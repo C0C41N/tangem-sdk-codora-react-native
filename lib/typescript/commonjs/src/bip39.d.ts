8e80c0e918b24a611a665655f3ff4be3f9fefc64
C0C41N <51693606+C0C41N@users.noreply.github.com>
2024-11-11 15:02:33 +0500

    chore: compile

---
+++ lib/typescript/commonjs/src/bip39.d.ts
@@ -0,0 +1,3 @@
+import type { WordCount } from './types';
+export declare function generateMnemonic(wordCount: WordCount): Promise<string[]>;
+//# sourceMappingURL=bip39.d.ts.map
--- lib/typescript/commonjs/src/types/index.d.ts
+++ lib/typescript/commonjs/src/types/index.d.ts
@@ -1,4 +1,5 @@
 export * from './backupSvc';
+export * from './bip39';
 export * from './card';
 export * from './operations';
 //# sourceMappingURL=index.d.ts.map
