06e44947603167ad9306237153522fdb244c8acb
C0C41N <51693606+C0C41N@users.noreply.github.com>
2024-11-24 23:45:03 +0500

    chore: path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  presets: [
-    ['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }],
+  presets: [['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }]],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@addressService': './src/addressService',
+          '@backupService': './src/backupService',
+          '@bip39': './src/bip39',
+          '@locale': './src/locale',
+          '@operations': './src/operations',
+
+          '@nativeModule': './src/nativeModule.ts',
+          '@types': './src/types.ts',
+        },
+      },
+    ],
   ],
 };
