54be956eb67b7c093435aa679daddcb3c12990ef
C0C41N <51693606+C0C41N@users.noreply.github.com>
2025-01-06 15:24:43 +0500

    chore: minor

--- lib/typescript/module/src/operations/operations.d.ts
+++ lib/typescript/module/src/operations/operations.d.ts
@@ -10,4 +10,5 @@ export declare function setAccessCode(params: ISetAccessCodeParams): Promise<INa
 export declare function resetBackup(params: IResetBackupParams): Promise<INativeResponse<void>>;
 export declare function resetCodes(params: IResetCodesParams): Promise<INativeResponse<void>>;
 export declare function enableBiometrics(enable: boolean): Promise<INativeResponse<void>>;
+export declare function deriveHDKey(): Promise<void>;
 //# sourceMappingURL=operations.d.ts.map
