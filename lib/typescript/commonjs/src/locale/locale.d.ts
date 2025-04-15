e8c9e744232d6431c226a92c7776e8ea52396faa
C0C41N <51693606+C0C41N@users.noreply.github.com>
2025-04-15 18:01:56 +0500

    chore: compile

--- lib/typescript/commonjs/src/locale/locale.d.ts
+++ lib/typescript/commonjs/src/locale/locale.d.ts
@@ -1,3 +1,4 @@
 import { LanguageCodes, type ISetAppLangResponse } from './types';
 export declare function setAppLanguage(languageCode: LanguageCodes): Promise<ISetAppLangResponse>;
+export declare function forceExitApp(): void;
 //# sourceMappingURL=locale.d.ts.map
