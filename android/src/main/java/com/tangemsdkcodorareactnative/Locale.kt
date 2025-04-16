022467761eba15198fc4914e2bc7c008484dd10b
C0C41N <51693606+C0C41N@users.noreply.github.com>
2025-04-16 15:46:27 +0500

    chore: minor

--- android/src/main/java/com/tangemsdkcodorareactnative/Locale.kt
+++ android/src/main/java/com/tangemsdkcodorareactnative/Locale.kt
@@ -1,37 +1,37 @@
 package com.tangemsdkcodorareactnative
 import android.content.Context
 import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReactApplicationContext
 import java.util.Locale
 
 @Suppress("DEPRECATION")
 class Locale (private val module: TangemModule) {
 
   fun setAppLanguage(
-    context: ReactApplicationContext,
+    context: Context,
     languageCode: String,
     promise: Promise
   ) { try {
 
-    val (language, country) = languageCode.split("-")
+    AppCompatDelegate.setApplicationLocales(LocaleListCompat.create(Locale.forLanguageTag(languageCode)))
 
-    println("languageCode_1 $languageCode") ///
+//    val (language, country) = languageCode.split("-")
 
-    val locale = Locale(language, country)
-    Locale.setDefault(locale)
-    val config = Configuration(context.resources.configuration)
-    config.setLocale(locale)
-    context.resources.updateConfiguration(config, context.resources.displayMetrics)
+//    println("languageCode_1 $languageCode") ///
+
+//    val locale = Locale(language, country)
+//    Locale.setDefault(locale)
+//    val config = Configuration(context.resources.configuration)
+//    config.setLocale(locale)
+//    context.resources.updateConfiguration(config, context.resources.displayMetrics)
 
     val currentLocale: Locale = context.resources.configuration.locales.get(0) ///
     println("Current locale: ${currentLocale.language}-${currentLocale.country}") ///
 
     val localizedString = context.getString(com.tangem.sdk.R.string.view_delegate_scan) ///
     println("Sample locale string: $localizedString") ///
-
-    val activity = context.currentActivity
-    activity?.runOnUiThread { activity.recreate() }
 
     promise.resolve(true)
 
